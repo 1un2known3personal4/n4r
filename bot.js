3c3abad15fa8302416651d2b4572db1ebd491e1b
Maofkasopfkcafopasf <mohamedpadr94@gmail.com>
2018-12-23 15:39:21 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+console.log("Welcome Again !");
+ 
+ client.on("guildMemberRemove", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+سلام عليكم يا قلبي 
+لو سمحت طالبق طلب تكقا ادخل السيرفر
+                               [ https://discord.gg/7P3aHCE ] 
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+**`)
+}).catch(console.error)
+
+})
+ 
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+سلام عليكم يا قلبي 
+لو سمحت طالبق طلب تكقا ادخل السيرفر
+                               [ https://discord.gg/7P3aHCE ] 
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+**`) 
+}).catch(console.error)
+})
+client.login('NDQwOTU4ODU3NTIzNDk0OTIy');
