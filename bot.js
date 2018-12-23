0e0addc13d6c6c04aaf1a536872484ab8aab0ed8
Maofkasopfkcafopasf <mohamedpadr94@gmail.com>
2018-12-23 15:58:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,20 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 console.log("Welcome Again !");
- 
- client.on("guildMemberRemove", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(` 
-**
-سلام عليكم يا قلبي 
-لو سمحت طالبق طلب تكقا ادخل السيرفر
-                               [ https://discord.gg/md5wc7M ] 
-الدعووة خاصة لك ي قلبي ... [ ${member}  ]
-**`)
-}).catch(console.error)
-
-})
- 
+  
 client.on("guildMemberAdd", member => {
   member.createDM().then(function (channel) {
   return channel.send(` 
